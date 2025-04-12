7b4b544a6c0efd2e3d244db5f7dbac260e5307c1
May Sunktong <pattaranan.sunktong@gmail.com>
2025-04-12 23:42:46 +0200

    Make Sidebar pushes contents space

--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Footer";
 import MainHeader from "../MainHeader";
 import Sidebar from "../Sidebar";
+import styles from "./layout.module.css"
 
 const MainLayout = () => {
   return (
     <>
       <MainHeader />
-      <main>
+      <main className={styles.main}>
         <Sidebar />
         <Outlet />
       </main>
