27b41d2549ca53a7272be6034cc958ec9c9ddce3
May Sunktong <pattaranan.sunktong@gmail.com>
2025-04-13 17:36:24 +0200

    Filter animals on sidebar for every group 🦅

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,10 +2,20 @@ import { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import styles from "./sidebar.module.css";
 import AnimalCard from "../AnimalCard";
-import { allAnimals } from "../../data/animals";
+import { allAnimals, mammals, birds, reptiles } from "../../data/animals";
+import { useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
+
+  const { pathname } = useLocation();
+
+  const animals = (() => {
+    if (pathname.includes("mammals")) return mammals;
+    if (pathname.includes("reptiles")) return reptiles;
+    if (pathname.includes("birds")) return birds;
+    return allAnimals;
+  })();
 
   return (
     <div
@@ -16,8 +26,12 @@ const Sidebar = () => {
       </button>
       {isOpen && (
         <div className={styles.content}>
-          {allAnimals.map((animal) => (
-            <AnimalCard key={animal.name} animal={animal} animalImage={animal.imageUrl} />
+          {animals.map((animal) => (
+            <AnimalCard
+              key={animal.name}
+              animal={animal}
+              animalImage={animal.imageUrl}
+            />
           ))}
         </div>
       )}
