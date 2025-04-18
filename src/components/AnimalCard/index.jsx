a2b270431a797196fae86787f788f2b7478880f9
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-04-18 23:33:09 +0200

    Double click to navigate back to /category

--- src/components/AnimalCard/index.jsx
+++ src/components/AnimalCard/index.jsx
@@ -1,23 +1,34 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import styles from "./animalcard.module.css";
 
 const AnimalCard = ({ animal }) => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const basePath = pathname.split("/")[1];
   const slug = animal.name.toLowerCase().replaceAll(" ", "-");
   const isCategoryPage = ["mammals", "birds", "reptiles"].includes(basePath);
   const linkTarget = isCategoryPage ? `/${basePath}/${slug}` : `/${slug}`;
-
   const isActive = pathname === linkTarget;
+
+  const handleClick = (e) => {
+    if (isActive) {
+      e.preventDefault();
+      if (isCategoryPage) {
+        navigate(`/${basePath}`);
+      } else {
+        navigate("/");
+      }
+    }
+  };
 
   return (
     <div className={styles.animalCardContainer}>
-      <Link to={linkTarget}>
+      <Link to={linkTarget} onClick={handleClick}>
         <div
           className={`${styles.animalCard} ${isActive ? styles.active : ""}`}
         >
-          <img src={animal.imageUrl} alt="animal" />
+          <img src={animal.imageUrl} alt={animal.name} />
           <p>{animal.name}</p>
         </div>
       </Link>
