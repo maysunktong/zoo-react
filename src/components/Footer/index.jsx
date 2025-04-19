7289e34c666a0c1881d4f155dfeca8ba0d47e5c2
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-04-19 12:58:36 +0200

    Finish footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,19 @@
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
+import Logo from "/assets/logo/logo.png";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer}>
+        <img src={Logo} alt="logo" width={100} />
+        <h3>Business Hours</h3>
+        <p>Monday - Friday: 8 AM - 10 PM</p>
+        <p>Saturday - Sunday: 9 AM - 11 PM</p>
+      </div>
+      <div className={styles.footerBottom}>
+        <p>&copy; 2025 The Crispy Kale | May Sunktong</p>
+      </div>
+    </div>
+  );
 };
 export default Footer;
